ecb379aca7790c2ca08624d4ce4b8de650f9c029
pinchazumos <pinchazumos@gmail.com>
2022-09-25 13:46:29 +0200

    prefabs now have difficulty var

--- Assets/Scripts/TileScript.cs
+++ Assets/Scripts/TileScript.cs
@@ -10,6 +10,8 @@ public class TileScript : MonoBehaviour
     public BezierSpline cameraPath;
 
     public DirectionEnum[] path;
+	
+	public int difficulty;
 
     // Start is called before the first frame update
     void Start()
