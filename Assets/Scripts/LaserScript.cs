8666c0526d0b5fa531894d8d477ae635ea23212a
unknown <daniel.hurtadoramirez314@gmail.com>
2022-09-25 21:00:03 +0200

    lasers

---
+++ Assets/Scripts/LaserScript.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserScript : MonoBehaviour
+{
+
+    public float timeOn = 1f;
+    public float timeOff = .5f;
+    public float offset = .1f;
+
+    public GameObject laserOn;
+    public GameObject laserOff;
+
+    bool wasPreviousOn = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        laserOn.SetActive(false);
+        laserOff.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeOff > 0f)
+        {
+            float thing = (Time.time + offset) % (timeOn + timeOff);
+            Debug.Log(thing);
+            if (thing < timeOn)
+            {
+                if (!wasPreviousOn)
+                {
+                    wasPreviousOn = true;
+                    laserOn.SetActive(true);
+                    laserOff.SetActive(false);
+                }
+            }
+            else
+            {
+                if (wasPreviousOn)
+                {
+                    wasPreviousOn = false;
+                    laserOn.SetActive(false);
+                    laserOff.SetActive(true);
+                }
+            }
+        }
+    }
+}
